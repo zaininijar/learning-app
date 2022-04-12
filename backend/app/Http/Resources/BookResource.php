7acb882574868ec841d9c902530688ff5a62d306
zaininijar <zaininijar7610@gmail.com>
2022-04-12 20:42:57 +0700

    books api (get)

---
+++ backend/app/Http/Resources/BookResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BookResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'price' => $this->price,
+            'image' => $this->image,
+            'createdAt' => $this->created_at,
+            'updatedAt' => $this->updated_at
+        ];
+    }
+}
--- backend/database/migrations/2022_04_12_124811_create_books_table.php
+++ backend/database/migrations/2022_04_12_124811_create_books_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->double('price');
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
